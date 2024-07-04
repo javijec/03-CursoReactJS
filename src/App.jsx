0971103c9c0a367efc68d8ca24bc37f49ffd0231
JaViJeC <javijec@gmail.com>
2024-07-04 12:02:00 -0300

    Itemlistcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <NavBar />
-      <ItemListContainer />
+      <ItemListContainer props={'Bienvenidos a JECTEC'} />
     </>
   );
 };
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,11 @@
-const ItemListContainer = () => {
-    return (
-        <>
-        ItemListContainer
-        </>
-    )
-}
+const ItemListContainer = ({ props }) => {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-2xl">{props}</h1>
+      </div>
+    </>
+  );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
