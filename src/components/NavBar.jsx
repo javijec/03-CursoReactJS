042e1fbe412aa626aaded813df1cd59727b46888
JaViJeC <javijec@gmail.com>
2024-07-04 12:03:32 -0300

    fix Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
           <img src={logo} alt="Logo" className="w-20 h-20 rounded-full mr-4" />
-          <ol className="hidden md:flex space-x-8 text-white font-semibold">
+          <ol className="flex space-x-8 text-white font-semibold">
             <li className="hover:text-gray-200 transition duration-300">uno</li>
             <li className="hover:text-gray-200 transition duration-300">dos</li>
             <li className="hover:text-gray-200 transition duration-300">tres</li>
